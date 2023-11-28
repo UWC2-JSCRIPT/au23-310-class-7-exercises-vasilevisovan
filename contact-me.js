39418665b5126dd1d1af343aac5c531e5ecee1fd
vasilevisovan <vasile.visovan@yahoo.com>
2023-11-28 05:52:04 -0800

    #4

--- contact-me.js
+++ contact-me.js
@@ -1,19 +1,5 @@
 // TODO
- 
-/*document.getElementById('inquiry-type').addEventListener('change', function() {
-    // Hide all additional sections initially
-    document.querySelector('.job-opportunity').classList.add('hidden');
-    document.querySelector('.talk-code').classList.add('hidden');
-
-    // Show the selected additional section
-    var selectedOption = this.value;
-    if (selectedOption === 'job-opportunity') {
-        document.querySelector('.job-opportunity').classList.remove('hidden');
-    } else if (selectedOption === 'talk-code') {
-        document.querySelector('.talk-code').classList.remove('hidden');
-    }
-});*/
-document.getElementById('inquiry-type').addEventListener('change', function() {
+ document.getElementById('inquiry-type').addEventListener('change', function() {
     // Hide all additional sections initially
     document.querySelector('.job-opportunity').classList.add('hidden');
     document.querySelector('.talk-code').classList.add('hidden');
@@ -26,3 +12,9 @@ document.getElementById('inquiry-type').addEventListener('change', function() {
         document.querySelector('.talk-code').classList.remove('hidden');
     }
 });
+
+
+
+
+
+
